c07544280bdd1306a4691396fd512d1e7fa15a0d
Fi <filip.m.wojciechowski@gmail.com>
2019-12-08 14:52:35 +0100

    Updates

---
+++ precache-manifest.1d6c5b7a3e3b7b45bd0262832f038a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "508f4b158b033fb8d0c0",
+    "url": "/Fefco/static/css/main.238d0ff2.chunk.css"
+  },
+  {
+    "revision": "508f4b158b033fb8d0c0",
+    "url": "/Fefco/static/js/main.7fc45b34.chunk.js"
+  },
+  {
+    "revision": "66a20d7e53aa51c393b2",
+    "url": "/Fefco/static/js/runtime~main.b5985acd.js"
+  },
+  {
+    "revision": "688f50d758f0748bf2a8",
+    "url": "/Fefco/static/js/2.6ad9597c.chunk.js"
+  },
+  {
+    "revision": "f6f1bb8aa97e21a3146c433e2632d1e2",
+    "url": "/Fefco/index.html"
+  }
+];
